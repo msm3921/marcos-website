a57240e0bf572e3a568b5900508f751886c3e4b2
msm3921 <msm3921@yahoo.com>
2018-01-21 19:15:41 -0500

    trying to get hero background as custom field

--- wp-content/themes/bootstrap2wordpress/template-parts/content-hero.php
+++ wp-content/themes/bootstrap2wordpress/template-parts/content-hero.php
@@ -1,26 +1,28 @@
 <?php 
 ///Custom Fields
-$course_url				= get_post_meta(7,'course_url',true);
-$button_text			= get_post_meta(7,'button_text',true);
+ $hero_background	= get_field('hero_background');
+ $hero_emblem      	= get_field('hero_emblem');
  ?>
 
-<!-- HERO -->
-<section id="hero" date-type="background" data-speed="5">
-	<article>
-		<div class="container clearfix">
-			<div class="row">
+<!-- HERO --> 
+		<section id="hero" date-type="background" data-speed="5">
+			<div class="container clearfix">
+					<div class="row">
+					<!-- Hero Image-->
+					<div class="col-sm-5">
+						<?php if(!empty($hero_emblem)) : ?>
 
-				<div class="col-sm-5">
-					<img src="<?php bloginfo('stylesheet_directory');?>/assets/img/MH-Emblem.png" alt="Bootstrap to WorldPress" class="logo">
-				</div> <!-- col -->
+						<img src="<?php echo $hero_emblem['url']; ?>" alt="<?php echo $hero_emblem['alt']; ?>">
 
-				<div class="col-sm-7 hero-text">
-					<h1><?php bloginfo('name') ?></h1>
-					<p class="lead"><?php bloginfo('description') ?></p>
-					</div><!-- price-timeline -->
-					<!-- <p><a class="btn btn-lg btn-danger" href=" <?php $course_url; ?> " role="button"> <?php $button_text; ?> </a></p> -->
-				</div> <!-- col -->
-			</div> <!-- row -->
-		</div> <!-- container -->	
-	</article>
-</section> <!-- hero -->
+			   			<?php endif; ?>
+					</div> <!-- col -->
+
+					<div class="col-sm-7 hero-text">
+						<h1><?php bloginfo('name') ?></h1>
+						<p class="lead"><?php bloginfo('description') ?></p>
+						</div><!-- price-timeline -->
+						<!-- <p><a class="btn btn-lg btn-danger" href=" <?php $course_url; ?> " role="button"> <?php $button_text; ?> </a></p> -->
+					</div> <!-- col -->
+					</div> <!-- row -->
+				</div> <!-- container -->	
+		</section>
